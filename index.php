9f00045dabc42f408247efdc0dfe5a518ee75fb5
Sehat Maruli Tua Samosir <if316020@students.del.ac.id>
2017-04-21 22:14:16 +0700

    Add new file

---
+++ index.php
@@ -0,0 +1,90 @@
+<?php
+	require_once(dirname(__FILE__).'/common/header.php');
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Mutiara Balige Hotel Information System</title>
+
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="css/font-awesome.min.css">
+    <link rel="stylesheet" href="css/animate.css">
+    <link href="css/animate.min.css" rel="stylesheet"> 
+    <link href="css/style.css" rel="stylesheet" />
+    
+    <script src="js/jquery.js"></script>
+    <script src="js/bootstrap.min.js"></script> 
+    <script src="js/wow.min.js"></script>
+    <script>wow = new WOW({}).init();</script>  
+</head>
+<body>
+  	<div class="slider">		
+		<div id="about-slider">
+			<div id="carousel-slider" class="carousel slide" data-ride="carousel">
+				<!-- Indicators -->
+				<ol class="carousel-indicators visible-xs">
+					<li data-target="#carousel-slider" data-slide-to="0" class="active"></li>
+					<li data-target="#carousel-slider" data-slide-to="1"></li>
+					<li data-target="#carousel-slider" data-slide-to="2"></li>
+				</ol>
+
+				<div class="carousel-inner">
+					<div class="item active">						
+						<img src="img/front-2.jpg" class="img-responsive" alt=""> 
+						<div class="carousel-caption">
+							<div class="wow fadeInUp" data-wow-offset="0" data-wow-delay="0.3s">								
+								<h2><span>Mutiara Balige Hotel</span></h2>
+							</div>
+							<div class="col-md-10 col-md-offset-1">
+								<div class="wow fadeInUp" data-wow-offset="0" data-wow-delay="0.6s">								
+									<p>Text Here</p>
+								</div>
+							</div>
+						</div>
+				    </div>
+			
+				    <div class="item">
+						<img src="img/rooftop.jpg" class="img-responsive" alt=""> 
+						<div class="carousel-caption">
+							<div class="wow fadeInUp" data-wow-offset="0" data-wow-delay="0.3s">								
+								<h2>Bring your happines</h2>
+							</div>
+							<div class="col-md-10 col-md-offset-1">
+								<div class="wow fadeInUp" data-wow-offset="0" data-wow-delay="0.6s">								
+									<p>Lorem ipsum dolor sit amet consectetur adipisicing</p>
+								</div>
+							</div>
+						</div>
+				    </div> 
+				    <div class="item">
+						<img src="img/kamar-superior.jpg" class="img-responsive" alt=""> 
+						<div class="carousel-caption">
+							<div class="wow fadeInUp" data-wow-offset="0" data-wow-delay="0.3s">								
+								<h2>Online Ordering</h2>
+							</div>
+							<div class="col-md-10 col-md-offset-1">
+								<div class="wow fadeInUp" data-wow-offset="0" data-wow-delay="0.6s">								
+									<p>Lorem ipsum dolor sit amet consectetur adipisicing</p>
+								</div>
+							</div>
+						</div>
+				    </div> 
+				</div>
+				
+				<a class="left carousel-control hidden-xs" href="#carousel-slider" data-slide="prev">
+					<i class="fa fa-angle-left"></i> 
+				</a>
+				
+				<a class=" right carousel-control hidden-xs" href="#carousel-slider" data-slide="next">
+					<i class="fa fa-angle-right"></i> 
+				</a>
+			</div>
+		</div>
+	</div>
+<?php
+		require_once(dirname(__FILE__).'/common/footer.php');
+?>
