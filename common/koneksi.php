9cf7b444fbf4e58fc45386e9dbf86b673d4c85fc
Sehat Maruli Tua Samosir <if316020@students.del.ac.id>
2017-05-11 21:14:48 +0700

    in progress | perubahan nama database | 11 Mei 2017

--- common/koneksi.php
+++ common/koneksi.php
@@ -2,7 +2,7 @@
 $host = "localhost";
 $user = "root";
 $pass = "";
-$name = "hotel2";
+$name = "p1d3ti03_hotel2";
  
 $koneksi = mysql_connect($host, $user, $pass) or die("Koneksi ke database gagal!");
 mysql_select_db($name, $koneksi) or die("Tidak ada database yang dipilih!");
