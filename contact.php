12ba92194ba163b68d60d151ee86f0af4d276d11
Sehat Maruli Tua Samosir <if316020@students.del.ac.id>
2017-05-19 20:54:04 +0700

    in progress | 19 Mei 2017

--- contact.php
+++ contact.php
@@ -1,3 +1,4 @@
+
 <?php
     require_once(dirname(__FILE__).'/common/header.php');
 ?>
@@ -7,93 +8,67 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-
-    <title>Contact</title>
-
+    <title>Contact Person</title>
     <link href="css/bootstrap.min.css" rel="stylesheet">
-	<link rel="stylesheet" href="css/font-awesome.min.css">
-	<link rel="stylesheet" href="css/animate.css">
-	<link href="css/animate.min.css" rel="stylesheet"> 
-	<link href="css/style.css" rel="stylesheet" />
-
+    <link rel="stylesheet" href="css/font-awesome.min.css">
+    <link rel="stylesheet" href="css/animate.css">
+    <link href="css/animate.min.css" rel="stylesheet"> 
+    <link href="css/style.css" rel="stylesheet" />
     <script src="js/jquery.js"></script>        
     <script src="js/bootstrap.min.js"></script> 
     <script src="https://maps.google.com/maps/api/js?sensor=true"></script>
     <script src="js/wow.min.js"></script>
     <script>wow = new WOW({}).init();</script>  
-
   </head>
   <body>
-	<section class="contact-page">
+    <section class="contact-page">
         <div class="container wow fadeInDown" data-wow-duration="1000ms" data-wow-delay="300ms">
-			<div class="page-header">
-            <div class="text-center">        
-                <h2></h2>
-                <p>Mutiara Balige Hotel is conveniently located in the strategic and central area of ​​Balige, approximately 18 KM from the airport
-                   Close to important Institutions and Spot such as recreational parks, tourist sites, offices, banks, schools
-                   As well as business locations such as markets and shops.
-                   We offer lodging panoramic views of Lake Toba, hills and rice fields.
-                </p>
-			</div>
-			</div>
-
-       <!-- Team -->
-        <div class="bg-color-sky-light">
-            <div class="content-lg container">
-                <div class="row margin-b-40">
-                    <div class="col-sm-6">
-                        
-                    </div>
+            <div class="page-header">
+                <div class="text-center">
+                    <h2>Contact Us</h2>
+                    <p>Mutiara Balige Hotel is conveniently located in the strategic and central area of ​​Balige, approximately 18 KM from Silangit Airport close to the Institution and important spots such as recreation parks, tourist attractions, offices, banks, schools and business locations such as markets and shops. We offer lodging panoramic views of Lake Toba, hills and rice fields.</p>
                 </div>
-                <!--// end row -->
-
-                <div class="row">
-                    <!-- Team -->
-                    <div class="col-sm-4 sm-margin-b-20">
-                        <div class="bg-color-white margin-b-20">
-                            <div class="wow zoomIn" data-wow-duration=".3" data-wow-delay=".1s">
-                                <img class="img-responsive" src="img/01.jpg" alt="Team Image">
-                            </div>
-                        </div>
-                        <h4><a href="#">Sehat Maruli Tua Samosir</a> <span class="text-uppercase margin-l-20">Project Manager</span></h4>
-                      
-                    </div>
-                    <!-- End Team -->
-
-                    <!-- Team -->
-                    <div class="col-sm-4 sm-margin-b-50">
-                        <div class="bg-color-white margin-b-20">
-                            <div class="wow zoomIn" data-wow-duration=".3" data-wow-delay=".1s">
-                                <img class="img-responsive" src="img/b.PNG" alt="Team Image">
-                            </div>
-                        </div>
-                        <h6><a href="#">Magdalena Simamora</a><br>
-                        <span class="text-uppercase margin-l-20">Lead Developer</span></h6>
-                       
-                    </div>
-                    <!-- End Team -->
-
-                    <!-- Team -->
-                    <div class="col-sm-4 sm-margin-b-50">
-                        <div class="bg-color-white margin-b-10">
-                            <div class="wow zoomIn" data-wow-duration=".3" data-wow-delay=".1s">
-                                <img class="img-responsive" src="img/a.PNG" alt="Team Image">
-                            </div>
-                        </div>
-                        <h6><a href="#">Putri A.E Sihotang</a> <br>
-                        <span class="text-uppercase margin-l-20">Designer</span></h6>
-                       
-                    </div>
-                    <!-- End Team -->
-                </div>
-                <!--// end row -->
             </div>
         </div>
-        <!-- End Team -->
+        <div class="container wow fadeInDown" data-wow-duration="1000ms" data-wow-delay="600ms">
+                <div class="col-md-3 wow fadeInDown" data-wow-duration="1000ms" data-wow-delay="300ms">
+                    
+                </div>
+                <div class="col-md-6 wow fadeInDown" data-wow-duration="1000ms" data-wow-delay="300ms">
+                <div class="text-center">
+                
+                     
+                    <table align="center">
+                                <tr>
+                                    <td rowspan="4"><img src="img/harryssimanungkalit.jpg" style="width: 200px; height: 200px; border-radius: 100%;">
+                                        <div class="text-center">
+                                            <h3>Operation Manager</h3>
+                                        </div>
+                                    </td>
+                                    <td><h5><i class="fa fa-id-badge" style="font-size: 20px"></i></h5></td>
+                                    <td><h5>Harrys Simanungkalit</h5></td>
+                                </tr>
+                                <tr>
+                                    <td><h5><i class="fa fa-facebook-official" style="font-size: 20px"></i></h5></td>
+                                    <td><h5>Harrys Simanungkalit</h5></td>
+                                </tr>
+                                <tr>
+                                    <td><h5><i class="fa fa-linkedin-square" style="font-size: 20px"></i></h5></td>
+                                    <td><h5>Harrys Simanungkalit</h5></td>
+                                </tr>
+                                <tr>
+                                    <td><h5><i class="fa fa-whatsapp" style="font-size: 20px"></i></h5></td>
+                                    <td><h5>0815-1462-7544</h5></td>
+                                </tr>
+                            </table>
+                </div>
+                        </div>
+                    </div>
+                </div>
+        </div>
     </section>    
 </body>
 </html>
-
 <?php
     require_once(dirname(__FILE__).'/common/footer.php');
 ?>
