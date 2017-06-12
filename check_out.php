ca63ada88544d5701eb7f530dc67c7b73543b7ba
Sehat Maruli Tua Samosir <if316020@students.del.ac.id>
2017-06-12 16:02:25 +0700

    closed | 12 Juni 2017

--- check_out.php
+++ check_out.php
@@ -1,14 +1,14 @@
 <?php
 	include(dirname(__FILE__).'/common/koneksi.php');
 	        
-	$orders	= $_GET['id'];
+    $orders = $_GET['id'];
 
-	$check_out = mysqli_query($koneksi, "UPDATE orders SET keterangan='Check Out' WHERE orders_id='$orders'");
-	$data_orders	= mysqli_fetch_array($select_orders);
-	$room_no		= $data_orders['room_no'];
-	$update_room    = mysqli_query($koneksi, "UPDATE room SET keterangan='Kosong' WHERE room_no='$room_no'");
-
-	if ($check_out && $update_room){
+    $check_out     = mysqli_query($koneksi, "SELECT * FROM orders WHERE orders_id='$orders'");
+    $data_orders    = mysqli_fetch_array($check_out);
+    $room_no        = $data_orders['room_no'];
+    $update_room    = mysqli_query($koneksi, "UPDATE room SET keterangan='Kosong' WHERE room_no='$room_no'");
+	
+    if ($check_out && $update_room){
 	    header('Location: order-list.php');
 	}else{
 	    require_once(dirname(__FILE__).'/common/header.php');
